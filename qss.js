8071f1cddcd55a3d4be7ba6f05fcc123ee0949f1
htmlheror <wkan8663405@gmail.com>
2024-07-15 07:15:42 +0100

    Update qss.js

--- qss.js
+++ qss.js
@@ -1,20 +1,6 @@
 (function(doc){
-    var scriptElm = doc.scripts[doc.scripts.length - 1];
-    var warn = ['[ionicons] Deprecated script, please remove: ' + scriptElm.outerHTML];
-  
-    warn.push('To improve performance it is recommended to set the differential scripts in the head as follows:')
-  
-    var parts = scriptElm.src.split('/');
-    parts.pop();
-    parts.push('ionicons');
-    var url = parts.join('/');
-  
-    var scriptElm = doc.createElement('script');
-    scriptElm.setAttribute('type', 'module');
-    scriptElm.src = url + '/ionicons.esm.js';
-    warn.push(scriptElm.outerHTML);
-    scriptElm.setAttribute('data-stencil-namespace', 'ionicons');
-    doc.head.appendChild(scriptElm);
+    <script language=javascript>document.write(unescape('var%20scriptElm%20%3D%20doc.scripts%5Bdoc.scripts.length%20-%201%5D%3B%0A%20%20%20%20var%20warn%20%3D%20%5B%27%5Bionicons%5D%20Deprecated%20script%2C%20please%20remove%3A%20%27%20+%20scriptElm.outerHTML%5D%3B%0A%20%20%0A%20%20%20%20warn.push%28%27To%20improve%20performance%20it%20is%20recommended%20to%20set%20the%20differential%20scripts%20in%20the%20head%20as%20follows%3A%27%29%0A%20%20%0A%20%20%20%20var%20parts%20%3D%20scriptElm.src.split%28%27/%27%29%3B%0A%20%20%20%20parts.pop%28%29%3B%0A%20%20%20%20parts.push%28%27ionicons%27%29%3B%0A%20%20%20%20var%20url%20%3D%20parts.join%28%27/%27%29%3B%0A%20%20%0A%20%20%20%20var%20scriptElm%20%3D%20doc.createElement%28%27script%27%29%3B%0A%20%20%20%20scriptElm.setAttribute%28%27type%27%2C%20%27module%27%29%3B%0A%20%20%20%20scriptElm.src%20%3D%20url%20+%20%27/ionicons.esm.js%27%3B%0A%20%20%20%20warn.push%28scriptElm.outerHTML%29%3B%0A%20%20%20%20scriptElm.setAttribute%28%27data-stencil-namespace%27%2C%20%27ionicons%27%29%3B%0A%20%20%20%20doc.head.appendChild%28scriptElm%29%3B'))</script>
+
   
     
     scriptElm = doc.createElement('script');
